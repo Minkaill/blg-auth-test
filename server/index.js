cf6791201ecfa1680f634f9cd231ad112f173bb2
Minkaill <mkx.dake@gmail.com>
2023-04-30 17:43:08 +0300

    Connect env

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+// Constants
+const PORT = process.env.PORT || 4001;
+const DB_SERVER = process.env.DB_SERVER;
 
 async function start() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://fringle:12345@cluster0.wgisrd8.mongodb.net/blog"
-    );
-    app.listen(4000, () => {
-      console.log(`Server started on port: ${4000}`);
+    await mongoose.connect(DB_SERVER);
+    app.listen(PORT, () => {
+      console.log(`Server started on port: ${PORT}`);
     });
   } catch (error) {
     console.log(error);

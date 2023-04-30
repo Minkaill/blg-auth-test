b1def53517deb44f80d6054ab33ca72c33fcdc53
Minkaill <mkx.dake@gmail.com>
2023-04-30 18:20:09 +0300

    Подключил cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -8,6 +9,10 @@ dotenv.config();
 // Constants
 const PORT = process.env.PORT || 4001;
 const DB_SERVER = process.env.DB_SERVER;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 async function start() {
   try {
